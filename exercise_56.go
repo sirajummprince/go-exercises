553a961d85775007ee482798be90d86ff9366382
Sirajum Munir Prince <sirajummunirprince@gmail.com>
2020-07-16 00:38:37 +0600

    WaitGroup

--- exercise_56.go
+++ exercise_56.go
@@ -3,8 +3,39 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
+var count int = 10
+var wg sync.WaitGroup
+
+func foo() {
+	for i := 0; i < count; i++ {
+		fmt.Println("foo:", i)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i := 0; i < count; i++ {
+		fmt.Println("bar:", i)
+	}
+	wg.Done()
+}
+
 func main() {
-	fmt.Println(runtime.NumCPU())
+	fmt.Println("OS:\t\t", runtime.GOOS)
+	fmt.Println("ARCH:\t\t", runtime.GOARCH)
+	fmt.Println("CPUs:\t\t", runtime.NumCPU())
+	fmt.Println("GoRoutines:\t", runtime.NumGoroutine())
+
+	wg.Add(2)
+	go foo()
+	go bar()
+	wg.Wait()
+
+	fmt.Println("OS:\t\t", runtime.GOOS)
+	fmt.Println("ARCH:\t\t", runtime.GOARCH)
+	fmt.Println("CPUs:\t\t", runtime.NumCPU())
+	fmt.Println("GoRoutines:\t", runtime.NumGoroutine())
 }
