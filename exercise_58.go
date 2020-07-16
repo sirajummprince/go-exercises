bd86f44515ad885b48d38dd9830cc09d32e916ff
Sirajum Munir Prince <sirajummunirprince@gmail.com>
2020-07-16 16:07:02 +0600

    Check race condition

---
+++ exercise_58.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+const gs = 100
+
+var counter int = 0
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("CPUs:\t\t", runtime.NumCPU())
+	fmt.Println("Counter:\t", counter)
+	fmt.Println("Go routines:\t", runtime.NumGoroutine())
+	fmt.Println("\n")
+
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Go routines:\t", runtime.NumGoroutine())
+	}
+	wg.Wait()
+
+	fmt.Println("Counter:\t", counter)
+	fmt.Println("Go routines:\t", runtime.NumGoroutine())
+}
+
+//To check `race` condition run `$ go run -race exercise_58.go`
