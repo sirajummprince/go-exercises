259895840b48625a5bdbfe463dfbed0868420501
Sirajum Munir Prince <sirajummunirprince@gmail.com>
2020-07-19 00:31:10 +0600

    Create custom error

--- exercise_71.go
+++ exercise_71.go
@@ -9,16 +9,16 @@ type customErr struct {
 }
 
 func (ce customErr) Error() string {
-	return fmt.Sprintf(ce.err)
+	return fmt.Sprintf("then received the error %v", ce.err)
 }
 
 func foo(e error) {
-	fmt.Println(e)
+	fmt.Println("foo ran first", e)
 }
 
 func main() {
 	c1 := customErr{
-		err: `Error occured in customErr`,
+		err: `finally printed the error`,
 	}
 	foo(c1)
 }
