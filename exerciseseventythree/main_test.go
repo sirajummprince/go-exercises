75b619827a8f7194ddab6a9753f0b2d27ed5e169
Sirajum Munir Prince <sirajummunirprince@gmail.com>
2020-07-20 01:23:38 +0600

    Test, and benchmark dog package with example

---
+++ exerciseseventythree/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sirajummprince/go-exercises/exerciseseventythree/dog"
+)
+
+func TestDog(t *testing.T) {
+	dy := dog.Years(10)
+	if dy != 70 {
+		t.Error("Something  went wrong. Expected", 70, "Got %v", dy)
+	}
+}
+
+func BenchmarkDOg(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		dog.Years(10)
+	}
+}
+
+func ExampleDog() {
+	dogYears := dog.Years(10)
+	fmt.Println(dogYears)
+	//Output:
+	//70
+}
